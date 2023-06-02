2305559445c42189aeab181517ce6dee0562fc6d
Andrej Bauer <Andrej.Bauer@andrej.com>
2023-06-02 09:12:30 +0200

    Free memory.

--- Main.lean
+++ Main.lean
@@ -1,13 +1,14 @@
 import Lean
 import Sexp
 
-def main (args : List String) : IO Unit := do
+unsafe def main (args : List String) : IO Unit := do
   let srcDir := System.FilePath.mk $ (args.get? 0).getD "build/lib"
   let outDir := System.FilePath.mk $ (args.get? 1).getD "sexp"
   IO.println s!"Extracting s-expressions from {srcDir} to {outDir}"
   IO.FS.createDirAll outDir
   let files ← System.FilePath.walkDir srcDir
-  for srcFile in files.filter (fun fp => fp.toString.endsWith ".olean") do
+  let totalFiles := files.size
+  for (k, srcFile) in (files.toList.filter (fun fp => fp.toString.endsWith ".olean")).enum do
     if ! srcFile.toString.startsWith srcDir.toString then
       IO.println s!"skipping {srcFile} because, mysteriously, is not in {outDir}"
     else
@@ -17,7 +18,8 @@ def main (args : List String) : IO Unit := do
       | .some stemFile =>
         let baseFile := System.FilePath.mk $ baseName.replace "/" "."
         let outFile := System.FilePath.join outDir (baseFile.withExtension "sexp")
-        IO.println s!"{srcFile} -> {outFile}"
-        let (data, _) ← Lean.readModuleData srcFile
+        IO.println s!"[{k}/{totalFiles}] {srcFile} -> {outFile}"
+        let (data, region) ← Lean.readModuleData srcFile
         let moduleName := (stemFile.splitOn "/").foldl Lean.Name.str Lean.Name.anonymous
         IO.FS.writeFile outFile $ toString $ Sexp.fromModuleData moduleName data
+        region.free
