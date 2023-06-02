2ea9b951889e57f441e23fbf255859d94e5a671c
Andrej Bauer <Andrej.Bauer@andrej.com>
2023-06-02 08:14:03 +0200

    First working version

--- Main.lean
+++ Main.lean
@@ -1,11 +1,24 @@
+import Lean
 import Sexp
 
 def main (args : List String) : IO Unit := do
-  let srcDir := args.get! 0
-  let outDir := args.get! 1
+  let srcDir := System.FilePath.mk (args.get! 0)
+  let outDir := System.FilePath.mk (args.get! 1)
   IO.println s!"Extracting s-expressions from {srcDir} to {outDir}"
-
--- def main (args : List String) : IO Unit := do
---   for fileName in args do
---     let (data, _) ← Lean.readModuleData fileName
---     IO.println $ Sexp.fromModuleData (.mkStr3 "fake" "module" "name") data
+  IO.FS.createDirAll outDir
+  let files ← System.FilePath.walkDir srcDir
+  --  (fun fp => do { let b ← fp.isDir ; pure $ (b || fp.toString.endsWith ".olean") })
+  for srcFile in files.filter (fun fp => fp.toString.endsWith ".olean") do
+    if ! srcFile.toString.startsWith srcDir.toString then
+      IO.println s!"skipping {srcFile} because, mysteriously, is not in {outDir}"
+    else
+      let baseName := srcFile.toString.drop (1 + srcDir.toString.length)
+      match srcFile.fileStem with
+      | .none => IO.println s!"skipping {srcFile} because, mysteriously, has no file extension"
+      | .some stemFile =>
+        let baseFile := System.FilePath.mk $ baseName.replace "/" "."
+        let outFile := System.FilePath.join outDir (baseFile.withExtension "sexp")
+        IO.println s!"{srcFile} -> {outFile}"
+        let (data, _) ← Lean.readModuleData srcFile
+        let moduleName := (stemFile.splitOn "/").foldl Lean.Name.str Lean.Name.anonymous
+        IO.FS.writeFile outFile $ toString $ Sexp.fromModuleData moduleName data
